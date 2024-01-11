3e8295900d0cc6440a468803062155ff8344c56d
VictorSZETO1 <156228405+VictorSZETO1@users.noreply.github.com>
2024-01-11 13:20:26 -0500

    It can now figure out what Colom the symbols are in

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 namespace SpriteKind {
     export const UI = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    curY = Math.max(0, curY - 1)
+    UpdateCursor()
+})
 function InitKeys () {
     for (let index = 0; index <= keypadList.length - 1; index++) {
         mySprite = sprites.create(keypadList[index], SpriteKind.UI)
@@ -12,61 +16,38 @@ function UpdateCursor () {
     cursorSprite.left = curX * 23
     cursorSprite.top = curY * 31
 }
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    curX = Math.max(0, curX - 1)
-    UpdateCursor()
-})
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (curX == 6) {
-        curY = Math.min(2, curY + 1)
-    } else {
-        curY = Math.min(3, curY + 1)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (SelectedCount == 4) {
+        find_colom()
+        if (incol1 == 4) {
+            game.splash("col1")
+        }
+        if (incol2 == 4) {
+            game.splash("col2")
+        }
+        if (incol3 == 4) {
+            game.splash("col3")
+        }
+        if (incol4 == 4) {
+            game.splash("col4")
+        }
+        if (incol5 == 4) {
+            game.splash("col5")
+        }
+        if (incol6 == 4) {
+            game.splash("col6")
+        }
+        InitKeys()
+        reset_value()
     }
-    UpdateCursor()
 })
-sprites.onCreated(SpriteKind.UI, function (sprite) {
-    sprite.setFlag(SpriteFlag.Ghost, true)
-})
-function clearScreen () {
-    sprite_list = sprites.allOfKind(SpriteKind.UI)
-    for (let value of sprite_list) {
-        value.destroy()
-    }
-}
-function InitCursor () {
-    cursorSprite = sprites.create(img`
-        8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8
-        8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . . . . . .
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 . . . . . . . . . . . . . . . . 8 8
-        8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8
-        8 8 8 8 8 . . . . . . . . . . 8 8 8 8 8
-    `, SpriteKind.UI)
-    UpdateCursor()
+function reset_value () {
+    incol1 = 0
+    incol2 = 0
+    incol3 = 0
+    incol4 = 0
+    incol5 = 0
+    incol6 = 0
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     current = curY * 7 + curX
@@ -86,6 +67,51 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     InitKeys()
     InitCursor()
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    curX = Math.max(0, curX - 1)
+    UpdateCursor()
+})
+function clearScreen () {
+    sprite_list = sprites.allOfKind(SpriteKind.UI)
+    for (let value of sprite_list) {
+        value.destroy()
+    }
+}
+function InitCursor () {
+    cursorSprite = sprites.create(img`
+        88888..........88888
+        88888..........88888
+        88................88
+        88................88
+        88................88
+        88................88
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        88................88
+        88................88
+        88................88
+        88................88
+        88................88
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        ....................
+        88................88
+        88................88
+        88................88
+        88888..........88888
+        88888..........88888
+        `, SpriteKind.UI)
+    UpdateCursor()
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (curY < 3) {
         curX = Math.min(6, curX + 1)
@@ -94,17 +120,152 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     }
     UpdateCursor()
 })
-function InitArray () {
-    Selections = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+function find_colom () {
+    if (Selections[0] == 1) {
+        incol1 += 1
+        incol2 += 1
+    }
+    if (Selections[1] == 1) {
+        incol1 += 1
+    }
+    if (Selections[2] == 1) {
+        incol1 += 1
+        incol3 += 1
+    }
+    if (Selections[3] == 1) {
+        incol1 += 1
+    }
+    if (Selections[4] == 1) {
+        incol1 += 1
+        incol4 += 1
+    }
+    if (Selections[5] == 1) {
+        incol1 += 1
+        incol2 += 1
+    }
+    if (Selections[6] == 1) {
+        incol2 += 1
+        incol1 += 1
+    }
+    if (Selections[7] == 1) {
+        incol2 += 1
+        incol6 += 1
+    }
+    if (Selections[8] == 1) {
+        incol2 += 1
+        incol3 += 1
+    }
+    if (Selections[9] == 1) {
+        incol2 += 1
+        incol3 += 1
+    }
+    if (Selections[10] == 1) {
+        incol2 += 1
+        incol4 += 1
+    }
+    if (Selections[11] == 1) {
+        incol3 += 1
+    }
+    if (Selections[12] == 1) {
+        incol3 += 1
+    }
+    if (Selections[13] == 1) {
+        incol3 += 1
+        incol4 += 1
+    }
+    if (Selections[14] == 1) {
+        incol3 += 1
+    }
+    if (Selections[15] == 1) {
+        incol4 += 1
+        incol6 += 1
+    }
+    if (Selections[16] == 1) {
+        incol4 += 1
+        incol5 += 1
+    }
+    if (Selections[17] == 1) {
+        incol4 += 1
+        incol5 += 1
+    }
+    if (Selections[18] == 1) {
+        incol4 += 1
+        incol5 += 1
+    }
+    if (Selections[19] == 1) {
+        incol6 += 1
+    }
+    if (Selections[20] == 1) {
+        incol5 += 1
+    }
+    if (Selections[21] == 1) {
+        incol5 += 1
+    }
+    if (Selections[22] == 1) {
+        incol5 += 1
+    }
+    if (Selections[23] == 1) {
+        incol6 += 1
+    }
+    if (Selections[24] == 1) {
+        incol6 += 1
+    }
+    if (Selections[25] == 1) {
+        incol6 += 1
+    }
 }
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    curY = Math.max(0, curY - 1)
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (curX == 6) {
+        curY = Math.min(2, curY + 1)
+    } else {
+        curY = Math.min(3, curY + 1)
+    }
     UpdateCursor()
 })
-let SelectedCount = 0
+function InitArray () {
+    Selections = [
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1
+    ]
+}
+sprites.onCreated(SpriteKind.UI, function (sprite) {
+    sprite.setFlag(SpriteFlag.Ghost, true)
+})
+let sprite_list: Sprite[] = []
 let Selections: number[] = []
 let current = 0
-let sprite_list: Sprite[] = []
+let incol6 = 0
+let incol5 = 0
+let incol4 = 0
+let incol3 = 0
+let incol2 = 0
+let incol1 = 0
+let SelectedCount = 0
 let cursorSprite: Sprite = null
 let mySprite: Sprite = null
 let curY = 0
